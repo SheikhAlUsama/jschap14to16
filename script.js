6efc61b1432214dce206474a0d002bb6331a60ba
SheikhAlUsama <82088182+SheikhAlUsama@users.noreply.github.com>
2021-05-02 18:33:38 +0500

    assignment

---
+++ script.js
@@ -0,0 +1,105 @@
+// var arr = [];
+
+
+// var arry = new Array();
+
+
+// var strg = ["red," , "yellow" , "Green"];
+// var num = [1, 2,3 ,4];
+
+// console.log(strg,num)
+
+
+// var bol = new boolean[size];
+
+
+// var mix = ["Banana", 3 ,"Apple" , 69 , "Kahjoor"];
+// console.log(mix)
+
+
+// var qualification = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil" , "PhD" ];
+// document.write("<h1> Qualofication <h1/>");
+// document.write("<h1>"+qualification[0] + "</h3>")
+// document.write("<h1>"+qualification[1] + "</h3>")
+// document.write("<h1>"+qualification[2] + "</h3>")
+// document.write("<h1>"+qualification[3] + "</h3>")
+// document.write("<h1>"+qualification[4] + "</h3>")
+// document.write("<h1>"+qualification[5] + "</h3>")
+// document.write("<h1>"+qualification[6] + "</h3>")
+// document.write("<h1>"+qualification[7] + "</h3>")
+
+
+
+
+// var student = ["Shakur", "Shakir","Talha"]
+// var score = [320 ,230 , 480]
+// document.write("<h2>" +"Score of " +student[0]+score[0] + "Percsntage 64%" +"</h2>")
+// document.write("<h2>" +"Score of "+student[1]+score[1] + "Percsntage 46%" +"</h2>")
+// document.write("<h2>" +"Score of "+student[2]+score[2] + "Percsntage 96%" +"</h2>")
+
+// var cites = ["Karachi","Lahore" ,"Islamabad" , "Quetta", "Peshawar"]
+// document.write(cites)
+
+// var newCites = cites.slice(2,4)
+// document.write(newCites)
+
+
+// var arr = ["This", "is" ,  "my" ,  "cat"];
+//  var newArr = arr.join(" ")
+//  console.log(newArr)
+
+
+// var device = []
+// device.unshift("keyboard")
+// device.unshift("mouse")
+// device.unshift("printer")
+// device.unshift("monitor")
+// console.log(device)
+// device.shift(1)
+// device.shift(1)
+// device.shift(1)
+// device.shift(1)
+// console.log(device)
+
+
+
+
+// var device = ["keyboard","Mouse","Printer","Monitor"]
+// device.pop()
+// console.log(device);
+// device.pop()
+// console.log(device);
+// device.pop()
+// console.log(device);
+// device.pop()
+// console.log(device);
+
+
+// var brands = ["Apple","Samsung", "Motorola", "Nokia", "Sony & Haier"]   
+// document.write("<select>" + "<option>"+ brands[0] +"</opton>"+ "<option>"+ brands[1] +"</opton>"+ "<option>"+ brands[2] +"</opton>" + "<option>"+ brands[3] +"</opton>"+ "<option>"+ brands[4] +"</opton>" +"</select>")
+
+
+
+
+// var arr = ["red","yellow", "green","blue"]
+// console.log(arr)
+// var addStart = prompt("Enter color name which you want to add start")
+// arr.unshift(addStart)
+// console.log(arr)
+// var addEnd = prompt("Enter color name which you want to add end")
+// arr.push(addEnd)
+// console.log(arr)
+// arr.unshift("Black","White")
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// var indx = + prompt("index num where you wnt to add color")
+// var color = prompt("enter color name")
+// arr.splice(indx,0,color)
+// console.log(arr)
+// var del = +prompt("enter where form delete")
+// var delHow = + prompt   ("How many delete")
+// arr.splice(del,delHow)
+// console.log(arr)
